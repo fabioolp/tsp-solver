c264ba6ce711026eaf2b35b9b88964d70df666ee
fabioolp <fabio.olp@gmail.com>
2023-08-30 17:12:48 -0300

    correções no calculo de distancia

--- src/utils.py
+++ src/utils.py
@@ -25,23 +25,25 @@ def calc_distancias(df, nome_index, nome_coord_x, nome_coord_y):
     coordenadas = df[[nome_coord_x, nome_coord_y]].values
 
     # Calcular as distâncias euclidianas entre as coordenadas usando cdist
-    distancias = cdist(coordenadas, coordenadas)
+    distancias_array = cdist(coordenadas, coordenadas, metric='euclidean')
+
+    # Outra forma de calcular a distancia euclidiana - sem método pronto
+    # distancias_array = np.zeros((len(coordenadas), len(coordenadas)))
+    # for i in range(len(coordenadas)):
+    #     for j in range(i + 1, len(coordenadas)):
+    #         distancias_array[i][j] = np.sqrt(((coordenadas[i][0] - coordenadas[j][0])**2) + ((coordenadas[i][1] - coordenadas[j][1])**2))
+    #         distancias_array[j][i] = distancias_array[i][j]
 
     # Criar um DataFrame a partir da matriz de distâncias
-    df_distancias = pd.DataFrame(distancias, columns=df[nome_index], index=df[nome_index])
+    df_distancias = pd.DataFrame(distancias_array, columns=df[nome_index], index=df[nome_index])
 
     return df_distancias
 
 
-# Função para calcular a distância entre duas cidades
-def calc_dist_euclidiana(cidade1, cidade2):
-    return np.sqrt((cidade1[0] - cidade2[0])**2 + (cidade1[1] - cidade2[1])**2)
-
-
 # Função para calcular a Função Objetivo (Distancia total percorrida)
 def calcula_fo(n, s, d):
-    distancia_total = sum(calc_dist_euclidiana(d[s[i]], d[s[i + 1]]) for i in range(n - 1))
-    distancia_total += calc_dist_euclidiana(d[s[-1]], d[s[0]])
+    distancia_total = sum(d[s[i]][s[i+1]] for i in range(n - 1))
+    distancia_total += d[s[-1]][s[0]]
 
     return distancia_total
 
